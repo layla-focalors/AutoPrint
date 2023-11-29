24fe27cf98d297540ba1251bdf114c8a53a3a212
Layla-focalors <starlights2283@gmail.com>
2023-11-29 22:43:27 +0900

    update file del method

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import shutil
+import time
 
 def GetFile():
     filePath = './cache'
@@ -14,21 +15,33 @@ def MakeLog(action, file_name):
         file.write(f"[{time}]file {file_name} is printed\n")
     elif(action == "copy"):
         file.write(f"[{time}]file {file_name} is backuped\n")
+    elif(action == "del"):
+        file.write(f"[{time}]file {file_name} is deleted\n")
     file.close()
     return None
 
 def PrintFile(file_list):
     file_Path = './cache'
+    names = []
     for i in file_list:
         CopyFile(i)
         os.startfile(file_Path + "\\"+ i, "print")
         MakeLog("print", i)
+        names.append(i)
+    time.sleep(5)
+    for j in names:
+        DropFile(j)
         
 def CopyFile(file_name):
     filePath = './cache'
     backup = './backup'
     shutil.copy(f'{filePath}//{file_name}', f'{backup}//{file_name}')
     MakeLog("copy", file_name)
+    
+def DropFile(file_name):
+    filePath = './cache'
+    os.remove(filePath + "//" + file_name)
+    MakeLog("del", file_name)
         
 if __name__ == "__main__":
     PrintFile(GetFile())
