937b9cc64986505616835cff8bd49a1754b6c4ed
Layla-focalors <starlights2283@gmail.com>
2023-11-29 22:37:14 +0900

    add backup method

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import shutil
 
 def GetFile():
     filePath = './cache'
@@ -7,18 +8,27 @@ def GetFile():
     return file_list
 
 def MakeLog(action, file_name):
+    time = datetime.datetime.now()
     file = open("log.txt", "a")
     if(action == "print"):
-        time = datetime.datetime.now()
         file.write(f"[{time}]file {file_name} is printed\n")
+    elif(action == "copy"):
+        file.write(f"[{time}]file {file_name} is backuped")
     file.close()
     return None
 
 def PrintFile(file_list):
     file_Path = './cache'
     for i in file_list:
+        CopyFile(i)
         os.startfile(file_Path + "\\"+ i, "print")
         MakeLog("print", i)
+        
+def CopyFile(file_name):
+    filePath = './cache'
+    backup = './backup'
+    shutil.copy(f'{filePath}//{file_name}', f'{backup}//{file_name}')
+    MakeLog("copy", file_name)
         
 if __name__ == "__main__":
     PrintFile(GetFile())
